cdedfdde545a0319d559e1ac44ea4548fd8c8c1d
pedroivodev <pedroivofe@gmail.com>
2019-04-05 08:07:21 -0300

    Criando primeiros componentes

---
+++ src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <ul>
+            <!-- <li v-bind:key="key" v-for="(item, key) in items">{{ item.name }}</li> -->
+            <li></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'List'
+}
+</script>
+
+<style lang="sass" scoped>
+ul
+    padding-left: 0
+    list-style: none
+    li
+        margin: 0px -5px
+        text-align: left
+        padding-left: 10px
+        padding: 5px 10px
+        border-top: 1px #000 solid
+        &:last-of-type
+            border-top: 1px #000 solid
+            border-bottom: 1px #000 solid
+</style>
+
---
+++ src/components/SearchableList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="search-box">
+            <input type="text" value="FGDFGDFafasfdasf">
+            <button>Pesquisar</button>
+        </div>
+         <!-- :items="items" -->
+        <List />
+    </div>
+</template>
+<script lang="ts">
+import List from './List.vue';
+
+export default {
+    name: 'SearchableList',
+    components: { List }
+}
+</script>
+
+<style lang="sass" scoped>
+.search-box
+    display: flex
+    height: 30px
+
+    input[type='text']
+        font-size: 16px
+        border-radius: 0px
+        border: 1px #2c3e50 solid
+        padding-left: 5px
+        @media (max-width: 400px)
+            width: 80%
+        &:focus
+            box-shadow: 0px 0px 1px 1px #42b983
+            z-index: 1
+
+    button
+        font-size: 13px
+        height: 100%
+        @media (max-width: 400px)
+            height: 100%
+            width: 20%
+
+</style>
+
