c4e7e0c4072023372beac069b545a99008997ba4
pedroivodev <pedroivofe@gmail.com>
2019-04-05 08:05:12 -0300

    Adicionando as telas iniciais do sistema

---
+++ src/views/External.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="wrapper">
+      <h1>Find stuff</h1>
+      <button @click="test">Teste</button>
+      <!-- :items="items"  -->
+      <SearchableList />
+   </div>
+</template>
+<script>
+import SearchableList from '../components/SearchableList.vue'
+
+export default {
+  name: 'External',
+  components: { SearchableList },
+  methods: {
+    test() {
+
+    }
+  }
+}
+</script>
+<style scope lang="sass">
+.wrapper
+  padding: 0px 5px
+</style>
---
+++ src/views/Shared.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="wrapper">
+      <h1>Shared stuff</h1>
+   </div>
+</template>
+<script>
+export default {
+  name: 'Shared'
+}
+</script>
+<style scope lang="sass">
+</style>
