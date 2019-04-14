acd63b6dbb405d615c7a15959a3b42718a212c68
pedroivodev <pedroivofe@gmail.com>
2019-04-14 12:04:41 -0300

    Adicionando props para componente básico de lista e adicionando css inicial

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,29 +1,46 @@
 <template>
     <div>
-        <ul>
-            <!-- <li v-bind:key="key" v-for="(item, key) in items">{{ item.name }}</li> -->
-            <li></li>
+        <ul class="list">
+            <li class="list-item" v-bind:key="key" v-for="(item, key) in items">
+              <div>
+                <div class="list-item__img"><img :src="item.thumbnail" /></div>
+                <div class="list-item__description">
+                  <div class="rating">{{ item.donatorRating }}</div>
+                  <div class="name">{{ item.name }}</div>
+                  <div class="quantity">Quantos? {{ item.quantity }}</div>
+                </div>
+              </div>
+            </li>
         </ul>
     </div>
 </template>
 <script>
 export default {
-    name: 'List'
+  name: 'List',
+  props: {
+    items: Object
+  }
 }
 </script>
 
 <style lang="sass" scoped>
 ul
-    padding-left: 0
-    list-style: none
-    li
-        margin: 0px -5px
-        text-align: left
+  padding-left: 0
+  list-style: none
+  li.list-item
+    margin: 0px -5px
+    text-align: left
+    border-top: 1px #000 solid
+    &:last-of-type
+      border-top: 1px #000 solid
+      border-bottom: 1px #000 solid
+    > div
+      display: flex
+      .list-item__img
+        > img
+          height: 100%
+          width: 100px
+      .list-item__description
         padding-left: 10px
         padding: 5px 10px
-        border-top: 1px #000 solid
-        &:last-of-type
-            border-top: 1px #000 solid
-            border-bottom: 1px #000 solid
 </style>
-
